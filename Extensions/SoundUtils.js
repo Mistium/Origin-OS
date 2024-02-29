bab624b641b0f23babccf50357091e248d9b6555
Mistium <92952823+Mistium@users.noreply.github.com>
2024-02-29 21:45:24 +0000

    Update SoundUtils.js

--- Extensions/SoundUtils.js
+++ Extensions/SoundUtils.js
@@ -57,6 +57,21 @@ class SoundUtils {
                             defaultValue: 'Channel 1'
                         }
                     }
+                },
+                {
+                    opcode: 'generateWhiteNoise',
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: 'Generate white noise on [CHANNEL] with frequency [FREQUENCY]Hz',
+                    arguments: {
+                        CHANNEL: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: 'Channel 1'
+                        },
+                        FREQUENCY: {
+                            type: Scratch.ArgumentType.NUMBER,
+                            defaultValue: 440
+                        }
+                    }
                 }
             ]
         };
@@ -117,6 +132,43 @@ class SoundUtils {
         }
 
         return channel.playing;
+    }
+    
+    generateWhiteNoise(args) {
+        const channelName = args.CHANNEL;
+        const frequency = args.FREQUENCY;
+        const channel = this.channels.find(ch => ch.name === channelName);
+        if (!channel) {
+            console.error(`Channel '${channelName}' not found.`);
+            return;
+        }
+
+        // Stop any existing source on the channel
+        if (channel.source) {
+            channel.source.stop();
+            channel.source = null;
+        }
+        channel.oscillator = this.audioContext.createOscillator();
+
+        const bufferSize = 4096;
+        const noiseBuffer = this.audioContext.createBuffer(1, bufferSize, this.audioContext.sampleRate);
+        const output = noiseBuffer.getChannelData(0);
+
+        for (let i = 0; i < bufferSize; i++) {
+            output[i] = Math.random() * 2 - 1;
+        }
+
+        const whiteNoise = this.audioContext.createBufferSource();
+        whiteNoise.buffer = noiseBuffer;
+        whiteNoise.loop = true;
+        whiteNoise.connect(this.audioContext.destination);
+        whiteNoise.start();
+
+        // Update frequency
+        const playbackRate = frequency / this.audioContext.sampleRate;
+        whiteNoise.playbackRate.setValueAtTime(playbackRate, this.audioContext.currentTime);
+
+        channel.source = whiteNoise;
     }
 }
 
