e2635d44c7e19c7167f2c443b28441422553ef92
Mistium <92952823+Mistium@users.noreply.github.com>
2024-08-05 04:08:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,15 @@ document.addEventListener('scroll', function() {
     image3.style.top = `calc(50% + ${newPosition* -1}vw)`;
     image3.style.left = `calc(50% + ${newPosition* -1}vw)`;
 });
+
+const image1 = document.getElementById('Image1');
+const image3 = document.getElementById('Image3');
+const scrollY = window.scrollY;
+
+const newPosition = scrollY / 50 + 5;
+
+image1.style.top = `calc(50% + ${newPosition}vw)`;
+image1.style.left = `calc(50% + ${newPosition}vw)`;
+
+image3.style.top = `calc(50% + ${newPosition* -1}vw)`;
+image3.style.left = `calc(50% + ${newPosition* -1}vw)`;
